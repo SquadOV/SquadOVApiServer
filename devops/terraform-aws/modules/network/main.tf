252453474e5349cdfb7f97c2f486ed4e996ef2a5
Michael Bao <mike@grchive.com>
2022-02-28 16:05:26 -0500

    fix ingress

--- devops/terraform-aws/modules/network/main.tf
+++ devops/terraform-aws/modules/network/main.tf
@@ -77,17 +77,18 @@ resource "aws_subnet" "k8s_subnet_public_a" {
     vpc_id = aws_vpc.primary.id
     availability_zone = "us-east-2a"
     cidr_block = "10.0.2.0/28"
-    
-    tags = {
-        "kubernetes.io/cluster/primary-eks-cluster" = "shared"
-        "kubernetes.io/role/elb" = "1"
-    }
 }
 
 resource "aws_subnet" "k8s_subnet_public_c" {
     vpc_id = aws_vpc.primary.id
     availability_zone = "us-east-2c"
     cidr_block = "10.0.2.32/28"
+}
+
+resource "aws_subnet" "k8s_subnet_public_a_v2" {
+    vpc_id = aws_vpc.primary.id
+    availability_zone = "us-east-2a"
+    cidr_block = "10.0.3.0/24"
     
     tags = {
         "kubernetes.io/cluster/primary-eks-cluster" = "shared"
@@ -95,6 +96,16 @@ resource "aws_subnet" "k8s_subnet_public_c" {
     }
 }
 
+resource "aws_subnet" "k8s_subnet_public_c_v2" {
+    vpc_id = aws_vpc.primary.id
+    availability_zone = "us-east-2c"
+    cidr_block = "10.0.4.0/24"
+    
+    tags = {
+        "kubernetes.io/cluster/primary-eks-cluster" = "shared"
+        "kubernetes.io/role/elb" = "1"
+    }
+}
 
 resource "aws_eip" "primary_nat_eip_a" {
     vpc = true
@@ -108,6 +119,18 @@ resource "aws_nat_gateway" "primary_nat_a" {
     depends_on = [ aws_internet_gateway.primary_gateway ]
 }
 
+resource "aws_eip" "primary_nat_eip_a_v2" {
+    vpc = true
+}
+
+resource "aws_nat_gateway" "primary_nat_a_v2" {
+    allocation_id = aws_eip.primary_nat_eip_a_v2.id
+    connectivity_type = "public"
+    subnet_id = aws_subnet.k8s_subnet_public_a_v2.id
+
+    depends_on = [ aws_internet_gateway.primary_gateway ]
+}
+
 resource "aws_eip" "primary_nat_eip_c" {
     vpc = true
 }
@@ -116,6 +139,18 @@ resource "aws_nat_gateway" "primary_nat_c" {
     allocation_id = aws_eip.primary_nat_eip_c.id
     connectivity_type = "public"
     subnet_id = aws_subnet.k8s_subnet_public_c.id
+
+    depends_on = [ aws_internet_gateway.primary_gateway ]
+}
+
+resource "aws_eip" "primary_nat_eip_c_v2" {
+    vpc = true
+}
+
+resource "aws_nat_gateway" "primary_nat_c_v2" {
+    allocation_id = aws_eip.primary_nat_eip_c_v2.id
+    connectivity_type = "public"
+    subnet_id = aws_subnet.k8s_subnet_public_c_v2.id
 
     depends_on = [ aws_internet_gateway.primary_gateway ]
 }
@@ -159,6 +194,16 @@ resource "aws_route_table_association" "k8s_public_rt_subnet_a" {
 resource "aws_route_table_association" "k8s_public_rt_subnet_c" {
     route_table_id = aws_route_table.public_route_table.id
     subnet_id = aws_subnet.k8s_subnet_public_c.id
+}
+
+resource "aws_route_table_association" "k8s_public_rt_subnet_a_v2" {
+    route_table_id = aws_route_table.public_route_table.id
+    subnet_id = aws_subnet.k8s_subnet_public_a_v2.id
+}
+
+resource "aws_route_table_association" "k8s_public_rt_subnet_c_v2" {
+    route_table_id = aws_route_table.public_route_table.id
+    subnet_id = aws_subnet.k8s_subnet_public_c_v2.id
 }
 
 resource "aws_vpc_endpoint" "s3_endpoint_us_east_2" {
